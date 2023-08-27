08e973059e51e27d216d224d4f4cc7ffa72c2e57
Mohammad Salah <thesalahrand@gmail.com>
2023-08-27 23:06:44 +0600

    feat: add update-single-cart-product api

---
+++ api/update-single-cart-product.php
@@ -0,0 +1,66 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: POST');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+use App\Config\Config;
+use App\Config\Database;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Cart;
+
+Config::init();
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db);
+$product = new Product($db);
+$cart = new Cart($db);
+
+$reqData = json_decode(file_get_contents('php://input'), true);
+require __DIR__ . '/validations/is-logged-in.validation.php';
+require __DIR__ . '/validations/update-single-cart-product.validation.php';
+
+$cart->productId = $product->id;
+$cart->userId = $user->id;
+
+$singleCartItem = $cart->readByUserAndProduct();
+
+switch ($reqData['type']) {
+  case 'increase':
+    if (!$singleCartItem) {
+      $cart->quantity = 1;
+      $singleCartItem = $cart->create();
+    } else {
+      $cart->id = $singleCartItem['id'];
+      $cart->quantity = $singleCartItem['quantity'] + 1;
+      $singleCartItem = $cart->updateQuantity();
+    }
+
+    break;
+  case 'decrease':
+    if (!$singleCartItem) {
+      http_response_code(404);
+      echo json_encode(['message' => 'Product not exists into the cart']);
+      exit();
+    }
+
+    $cart->id = $singleCartItem['id'];
+    if ($singleCartItem['quantity'] > 1) {
+      $cart->quantity = $singleCartItem['quantity'] - 1;
+      $singleCartItem = $cart->updateQuantity();
+    } else {
+      $singleCartItem = $cart->delete();
+    }
+
+    break;
+}
+
+http_response_code(200);
+echo json_encode($singleCartItem);
+?>
---
+++ models/Cart.php
@@ -0,0 +1,115 @@
+<?php
+namespace App\models;
+
+class Cart
+{
+  private $conn;
+  private $tableName = 'cart';
+
+  public $id;
+  public $userId;
+  public $productId;
+  public $quantity;
+  public $createdAt;
+  public $updatedAt;
+
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+
+  public function read()
+  {
+    $query = "SELECT `p`.`id`, `p`.`name`, `p`.`price`, `p`.`image`, `c`.`quantity` FROM `$this->tableName` `c` JOIN `products` `p` ON `c`.`product_id` = `p`.`id` WHERE `c`.`user_id` = :userId;";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':userId', $this->userId);
+
+    $stmt->execute();
+
+    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+  }
+
+  public function readById()
+  {
+    $query = "SELECT `id`, `user_id`, `product_id`, `quantity` FROM `$this->tableName` WHERE `id` = :id;";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':id', $this->id);
+
+    $stmt->execute();
+
+    return $stmt->fetch(\PDO::FETCH_ASSOC);
+  }
+
+  public function readByUserAndProduct()
+  {
+    $query = "SELECT `id`, `user_id`, `product_id`, `quantity` FROM `$this->tableName` WHERE `user_id` = :userId AND `product_id` = :productId;";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':userId', $this->userId);
+    $stmt->bindParam(':productId', $this->productId);
+
+    $stmt->execute();
+
+    return $stmt->fetch(\PDO::FETCH_ASSOC);
+  }
+
+  public function create()
+  {
+    $this->createdAt = date('Y-m-d H:i:s');
+    $this->updatedAt = date('Y-m-d H:i:s');
+
+    $query = "INSERT INTO `$this->tableName`(`user_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES(:userId, :productId, :quantity, :createdAt, :updatedAt);";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':userId', $this->userId);
+    $stmt->bindParam(':productId', $this->productId);
+    $stmt->bindParam(':quantity', $this->quantity);
+    $stmt->bindParam(':createdAt', $this->createdAt);
+    $stmt->bindParam(':updatedAt', $this->updatedAt);
+
+    $stmt->execute();
+
+    $this->id = $this->conn->lastInsertId();
+
+    return $this->readById();
+  }
+
+  public function updateQuantity()
+  {
+    $this->updatedAt = date('Y-m-d H:i:s');
+
+    $query = "UPDATE `$this->tableName` SET `quantity` = :quantity, `updated_at` = :updatedAt WHERE `id` = :id;";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':quantity', $this->quantity);
+    $stmt->bindParam(':updatedAt', $this->updatedAt);
+    $stmt->bindParam(':id', $this->id);
+
+    $stmt->execute();
+
+    return $this->readById();
+  }
+
+  public function delete()
+  {
+    $singleCartItem = $this->readById();
+
+    $query = "DELETE FROM `$this->tableName` WHERE `id` = :id;";
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':id', $this->id);
+
+    $stmt->execute();
+
+    return $singleCartItem;
+  }
+}
+?>
